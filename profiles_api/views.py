11bf65c09c3ea0461c365e7ec2fec8b6bf2f9288
Daniel Otieno <danielotieno.ke@gmail.com>
2019-03-26 21:00:12 +0300

    Create Hello api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test Api View."""
+
+    def get(self, request, format=None):
+        """Returns list of API views features."""
+
+        an_apiview = [
+            'Daniel', 'Rita', 'Moses', 'Kevin', 'Bev'
+        ]
+
+        return Response(
+            {
+                'message': 'Hello People!',
+                'an_apiview': an_apiview
+            }
+        )
