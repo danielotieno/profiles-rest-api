8a0b17800cda2657e455ab0ba50373ccecadf500
Daniel Otieno <danielotieno.ke@gmail.com>
2019-03-27 15:17:49 +0300

    Create Login ViewSet

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets, status, filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 
 from . import serializers
@@ -102,13 +104,19 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handles Creating,reading and updating profiles."""
 
     serializer_class = serializers.UserProfileSerializer
-
     queryset = models.UserProfile.objects.all()
-
     authentication_classes = (TokenAuthentication,)
-
     permissions_classes = (permissions.UpdateOwnProfile,)
-
     filter_backends = (filters.SearchFilter,)
-
     search_fields = ('name', 'email',)
+
+
+class LoginViewSet(viewsets.ViewSet):
+    """Checks email, password and returns authtoken."""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """Use the ObtainAuthToken APIView to validate and obtain token."""
+
+        return ObtainAuthToken().post(request)
